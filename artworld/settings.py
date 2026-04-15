4a5a64f10d2be647d7a462695fab440ee8ac0bae
king <funombawa2004@gmail.com>
2026-04-15 15:31:14 +0100

    install restframework

--- artworld/settings.py
+++ artworld/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'kingart',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- kingart/views.py
+++ kingart/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render, get_object_or_404
 from . models import Blog
-
+from django.http import JsonResponse
 
 # Create your views here.
 
 def test(request):
-    blog = Blog.objects.all()
-    return render(request, 'test.html', {'blogs':blog})
+    # blog = Blog.objects.all()
+    # return render(request, 'test.html', {'blogs':blog})
 
+    #PYTHON API
+    blog = Blog.objects.all().values('id','title','content')
+    print(blog)
+    # return JsonResponse('hello', safe=False)
+    return JsonResponse(list(blog), safe=False)
 
 def read(request,id):
     post = get_object_or_404(Blog, id=id)
